ae9155b5a2958cf802a0b448db3d211cab532d0f
Victor <rusuvictor30@gmail.com>
2020-08-04 16:51:19 -0400

    Last

--- src/components/Post.js
+++ src/components/Post.js
@@ -30,38 +30,38 @@ class Post extends Component {
     render(){
         return(
             <>
-            {  !this.state.postSubmitted ? 
-                (<div className="container">
-                    <div class="jumbotron mt-3">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <div className="well well-sm">
-                                    <form className="form-horizontal" method="post">
-                                        <fieldset>
-                                            <legend className="text-center header">Add new Post</legend>
-                                            <div className="form-group">
-                                                <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon"></i></span>
-                                                <input id="fname" onChange={this.onChange('title')} name="name" type="text" placeholder="Post Title" className="form-control" />
-                                            </div>
-                                            <div className="form-group">
-                                                <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
-                                                <textarea onChange={this.onChange('content')} className="form-control" id="message" name="message" placeholder="Enter your text here" rows="7"></textarea>
-                                            </div>
-                                            <div className="form-group">
-                                                <button type="submit" onClick={this.sunmitPost} className="btn btn-primary btn-lg">Submit</button>
-                                            </div>
-                                        </fieldset>
-                                    </form>
+                {  !this.state.postSubmitted ? 
+                    (<div className="container">
+                        <div class="jumbotron mt-3">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="well well-sm">
+                                        <form className="form-horizontal" method="post">
+                                            <fieldset>
+                                                <legend className="text-center header">Add new Post</legend>
+                                                <div className="form-group">
+                                                    <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon"></i></span>
+                                                    <input id="fname" onChange={this.onChange('title')} name="name" type="text" placeholder="Post Title" className="form-control" />
+                                                </div>
+                                                <div className="form-group">
+                                                    <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
+                                                    <textarea onChange={this.onChange('content')} className="form-control" id="message" name="message" placeholder="Enter your text here" rows="7"></textarea>
+                                                </div>
+                                                <div className="form-group">
+                                                    <button type="submit" onClick={this.sunmitPost} className="btn btn-primary btn-lg">Submit</button>
+                                                </div>
+                                            </fieldset>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>) : (
-                    <PDFViewer>
-                        <PDF title={this.state.title} content={this.state.content} />
-                    </PDFViewer>
-                )
-            }
+                    </div>) : (
+                        <PDFViewer>
+                            <PDF title={this.state.title} content={this.state.content} />
+                        </PDFViewer>
+                    )
+                }
             </>
         );
     }
